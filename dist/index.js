e15d2614edb281c096394aa0136b8fc6340ab206
zamasu <zxm273293@gmail.com>
2025-11-11 17:41:21 +0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+import { initializeDatabase } from './providers/appDataSource.js';
+import botCLient from "./providers/client.js";
+export const AppDataSource = await initializeDatabase();
+export const client = botCLient;
